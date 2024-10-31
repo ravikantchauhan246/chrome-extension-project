ec0fabb5f927a434ae3a522f9f3ee1d697d8f959
Ravikant Chauhan <53376708+ravikantchauhan246@users.noreply.github.com>
2024-10-31 21:03:21 +0530

    resolve save tab error

--- index.js
+++ index.js
@@ -14,9 +14,13 @@ if (linksFromLocalStorage) {
 
 tabBtn.addEventListener("click", function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLinks.push(tabs[0].url)
-        localStorage.setItem("myLinks", JSON.stringify(myLinks) )
-        render(myLinks)
+        if (tabs.length > 0 && tabs[0].url) {
+            myLinks.push(tabs[0].url)
+            localStorage.setItem("myLinks", JSON.stringify(myLinks))
+            render(myLinks)
+        } else {
+            console.error("No active tab or URL is undefined.")
+        }
     })
 })
 
@@ -24,9 +28,14 @@ tabBtn.addEventListener("click", function() {
 function render(links) {
     let listItems = ""
     for (let i = 0; i < links.length; i++) {
+        let url = links[i]
+        if (!url.startsWith('http://') && !url.startsWith('https://')) {
+            url = 'https://' + url
+        }
+        
         listItems += `
             <li>
-                <a target='_blank' href='${links[i]}'>
+                <a target='_blank' href='${url}'>
                     ${links[i]}
                 </a>
             </li>
@@ -42,8 +51,12 @@ deleteBtn.addEventListener("dblclick", function() {
 })
 
 inputBtn.addEventListener("click", function() {
-    myLinks.push(inputEl.value)
+    let url = inputEl.value
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+        url = 'https://' + url
+    }
+    myLinks.push(url)
     inputEl.value = ""
-    localStorage.setItem("myLinks", JSON.stringify(myLinks) )
+    localStorage.setItem("myLinks", JSON.stringify(myLinks))
     render(myLinks)
 })
