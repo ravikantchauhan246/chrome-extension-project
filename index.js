af17b29ce70a1327e95b42f99acf3c07301a9e4f
Ravikant Chauhan <53376708+ravikantchauhan246@users.noreply.github.com>
2024-10-31 20:32:19 +0530

    add deleteall button

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-let myLeads = []
+let myLinks = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const linksFromLocalStorage = JSON.parse( localStorage.getItem("myLinks") )
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+if (linksFromLocalStorage) {
+    myLinks = linksFromLocalStorage
+    render(myLinks)
+}
 
-function renderLeads() {
+// Refector the function so that it takes a parameter, leads, that it uses
+// instead of the global myLeads variable. Remember to update all invocations 
+// of the function as well.
+
+function render(links) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < links.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${links[i]}'>
+                    ${links[i]}
                 </a>
             </li>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLinks = []
+    render(myLinks)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLinks.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLinks", JSON.stringify(myLinks) )
+    render(myLinks)
+})
